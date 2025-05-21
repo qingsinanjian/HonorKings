d8a2f053a21ec4bfa4fb509dbf6d7f75a92e15be
CN\ex_wujx13 <ex_wujx13@partner.midea.com>
2025-05-21 10:49:13 +0800

    客户端发送消息及回调

--- Assets/Scripts/NetFramework/NetManager.cs
+++ Assets/Scripts/NetFramework/NetManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Sockets;
 using UnityEngine;
 
@@ -11,14 +12,21 @@ public static class NetManager
     /// 字节数组
     /// </summary>
     private static ByteArray byteArray;
-
+    /// <summary>
+    /// 消息列表
+    /// </summary>
     private static List<MsgBase> msgList;
+    /// <summary>
+    /// 发送队列
+    /// </summary>
+    private static Queue<ByteArray> writeQueue;
 
     private static void Init()
     {
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         byteArray = new ByteArray();
         msgList = new List<MsgBase>();
+        writeQueue = new Queue<ByteArray>();
     }
 
     public static void Connect(string ip, int port)
@@ -63,7 +71,7 @@ public static class NetManager
             //接受的数据量
             int count = socket.EndReceive(ar);
             //断开连接
-            if(count == 0)
+            if (count == 0)
             {
                 Close();
                 return;
@@ -74,7 +82,7 @@ public static class NetManager
             //处理消息
             OnReceiveData();
             //如果长度过小，扩容
-            if(byteArray.Remain < 8)
+            if (byteArray.Remain < 8)
             {
                 byteArray.MoveBytes();
                 byteArray.ReSize(byteArray.bytes.Length * 2);
@@ -89,7 +97,7 @@ public static class NetManager
 
     private static void Close()
     {
-        
+
     }
 
     /// <summary>
@@ -97,7 +105,7 @@ public static class NetManager
     /// </summary>
     private static void OnReceiveData()
     {
-        if(byteArray.Length <= 2)
+        if (byteArray.Length <= 2)
         {
             return;
         }
@@ -114,7 +122,7 @@ public static class NetManager
         int nameCount = 0;
         string protoName = MsgBase.DecodeName(byteArray.bytes, byteArray.readIndex, out nameCount);
 
-        if(string.IsNullOrEmpty(protoName))
+        if (string.IsNullOrEmpty(protoName))
         {
             Debug.LogError("协议名为空");
             return;
@@ -130,7 +138,7 @@ public static class NetManager
         byteArray.MoveBytes();
 
         //添加到消息列表
-        lock(msgList)
+        lock (msgList)
         {
             msgList.Add(msgBase);
         }
@@ -139,6 +147,79 @@ public static class NetManager
         if (byteArray.Length > 2)
         {
             OnReceiveData();
+        }
+    }
+
+    /// <summary>
+    /// 发送协议
+    /// </summary>
+    /// <param name="msgBase"></param>
+    public static void Send(MsgBase msgBase)
+    {
+        if(socket == null || !socket.Connected)
+        {
+            Debug.LogError("Socket未连接");
+            return;
+        }
+        //编码
+        byte[] nameBytes = MsgBase.EncodeName(msgBase);
+        byte[] bodyBytes = MsgBase.Encode(msgBase);
+        //消息长度
+        int length = nameBytes.Length + bodyBytes.Length;
+        //消息体
+        byte[] sendBytes = new byte[2 + length];
+        sendBytes[0] = (byte)(length % 256);
+        sendBytes[1] = (byte)(length / 256);
+        Array.Copy(nameBytes, 0, sendBytes, 2, nameBytes.Length);
+        Array.Copy(bodyBytes, 0, sendBytes, 2 + nameBytes.Length, bodyBytes.Length);
+        
+        ByteArray byteArray = new ByteArray(sendBytes);
+        int count = 0;
+        lock(writeQueue)
+        {
+            writeQueue.Enqueue(byteArray);
+            count = writeQueue.Count;
+        }
+        if(count == 1)
+        {
+            socket.BeginSend(byteArray.bytes, 0, byteArray.Length, SocketFlags.None, SendCallback, socket);
+        }
+    }
+
+    /// <summary>
+    /// 发送回调函数
+    /// </summary>
+    /// <param name="ar"></param>
+    private static void SendCallback(IAsyncResult ar)
+    {
+        Socket socket = ar.AsyncState as Socket;
+        if(socket == null || !socket.Connected)
+        {
+            return;
+        }
+        int count = socket.EndSend(ar);
+
+        ByteArray ba;
+        lock(writeQueue)
+        {
+            ba = writeQueue.First();
+        }
+
+        ba.readIndex += count;
+        //如果发送完毕，移除队列
+        if(ba.Length == 0)
+        {
+            lock(writeQueue)
+            {
+                //清除
+                writeQueue.Dequeue();
+                //取到下一个
+                ba = writeQueue.First();
+            }
+        }
+        if(ba != null)
+        {
+            socket.BeginSend(ba.bytes, ba.readIndex, ba.Length, SocketFlags.None, SendCallback, socket);
         }
     }
 }
